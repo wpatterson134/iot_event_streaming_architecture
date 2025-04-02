1c4fbd5689e7b0719cc453c1a98b00bd576aa04a
wpatterson134 <wpatterson134@gmail.com>
2025-04-02 02:13:09 -0700

    Importing configurationproperties class

--- microservices/iot_processor/src/main/java/com/dreamsoftware/iotframesingest/IotframesingestApplication.java
+++ microservices/iot_processor/src/main/java/com/dreamsoftware/iotframesingest/IotframesingestApplication.java
@@ -2,6 +2,8 @@ package com.dreamsoftware.iotframesingest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties(Properties.class)
